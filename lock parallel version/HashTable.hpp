bc3ee0d05b159333f7bd0ac2ef3fc9c5f7c93540
Diegogabo <diego.gaboardi@mail.polimi.it>
2017-10-01 16:53:13 +0200

    parallel version of hash table with lock

---
+++ lock parallel version/HashTable.hpp
@@ -0,0 +1,35 @@
+#include <Eigen/Dense>
+#include <vector>  
+#include <string>
+
+using namespace Eigen;
+
+/*a single entry of the hash table*/
+class HashEntry
+{
+	private:
+		std::vector<Nucleotide> key;
+		int count;
+	public:
+		HashEntry();
+		bool isEmpty();
+		int getC();
+		std::vector<Nucleotide> getK();
+		void setC(int);
+		void setK(std::vector<Nucleotide>);
+		std::string toString();
+};
+
+class HashTable
+{
+	private:
+		int k;
+		std::vector<HashEntry> table;
+		MatrixXf matrix, inverse;
+		int f(std::vector<Nucleotide>); /*TODO*/
+		int reprobe(int);
+	public:
+		HashTable(int); 
+		void incrementValue(std::vector<Nucleotide>);
+		std::string toString();
+};
